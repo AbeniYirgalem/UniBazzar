fedca059198dac688ecafd3bdeaa6e24caa5814a
Abeni <abenezeryirgalem0@gmail.com>
2026-01-03 22:52:12 +0300

    feat(auth): implement RegisterUser use case for user registration

---
+++ unibazzar/lib/features/auth/domain/usecases/register_user.dart
@@ -0,0 +1,16 @@
+import '../entities/user.dart';
+import '../repositories/auth_repository.dart';
+
+class RegisterUser {
+  const RegisterUser(this.repository);
+
+  final AuthRepository repository;
+
+  Future<User> call({
+    required String name,
+    required String email,
+    required String password,
+  }) {
+    return repository.register(name: name, email: email, password: password);
+  }
+}
