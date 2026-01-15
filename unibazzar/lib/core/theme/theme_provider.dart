c28d5bde0f2eeb89551c2d65eccc40d4e2e01fd4
Abeni <abenezeryirgalem0@gmail.com>
2026-01-15 23:15:43 +0300

    feat: implement ThemeController to manage ThemeMode with Riverpod StateNotifier

---
+++ unibazzar/lib/core/theme/theme_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ThemeController extends StateNotifier<ThemeMode> {
+  ThemeController() : super(ThemeMode.dark);
+
+  void toggle() {
+    state = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+  }
+
+  void setMode(ThemeMode mode) {
+    state = mode;
+  }
+}
+
+final themeModeProvider = StateNotifierProvider<ThemeController, ThemeMode>(
+  (ref) => ThemeController(),
+);
